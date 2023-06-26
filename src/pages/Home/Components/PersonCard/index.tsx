3eca6aca975dc4cea758fbd0cbb8ed4c66408fa2
Gabriel <gabrielloures21@gmail.com>
2023-06-26 08:59:48 -0300

    End PersonalCardInfos

--- src/pages/Home/Components/PersonCard/index.tsx
+++ src/pages/Home/Components/PersonCard/index.tsx
@@ -1,35 +1,62 @@
 import { PersonCardContainer, PersonalBaseInfos, PersonalInfos, PersonalInfosFooter, PersonalInfosHeader } from "./styles";
 
-import { ArrowSquareOut, GithubLogo, Buildings, Users } from "phosphor-react";
+import { ArrowSquareOut, GithubLogo, Buildings, ShareNetwork } from "phosphor-react";
+
+import { api } from "../../../../lib/axios";
+import { useEffect, useState } from "react";
+
+interface PersonalGithubInfos {
+  name: string
+  html_url: string
+  bio: string
+  login: string
+  location: string
+  public_repos: number
+  avatar_url: string
+}
 
 export function PersonCard() {
+
+  const [user, setUser] = useState<PersonalGithubInfos>()
+
+  async function getPersonalGithubInfos() {
+
+    const res = await api.get('/users/gabrielloures')
+
+    setUser(res.data)
+  }
+
+  useEffect(() => {
+    getPersonalGithubInfos()
+  }, [])
+
   return (
     <PersonCardContainer>
-        <img src="https://avatars.githubusercontent.com/u/108671628?v=4" alt="Imagem do usuário" />
+        <img src={user?.avatar_url} alt="Imagem do usuário" />
 
         <PersonalInfos>
           <PersonalInfosHeader>
-            <h1>Gabriel Loures</h1>
-            <a href="http://github.com/GabrielLoures" target="blank">
+            <h1>{user?.name}</h1>
+            <a href={user?.html_url} target="blank">
               <span>Github</span>
               <ArrowSquareOut size={16} weight="bold"/>
             </a>
           </PersonalInfosHeader>    
 
-          <p>Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat pulvinar vel mass.</p>
+          <p>{user?.bio}</p>
 
           <PersonalInfosFooter>
             <PersonalBaseInfos>
               <GithubLogo size={18} color="#3A536B"/>
-              <span>GabrielLoures</span>
+              <span>{user?.name}</span>
             </PersonalBaseInfos>
             <PersonalBaseInfos>
               <Buildings size={18} color="#3A536B"/>
-              <span>Rocketseat</span>
+              <span>{user?.location}</span>
             </PersonalBaseInfos>
             <PersonalBaseInfos>
-              <Users size={18} color="#3A536B"/>
-              <span>33 seguidores</span>
+              <ShareNetwork size={18} color="#3A536B"/>
+              <span>{user?.public_repos} repositórios</span>
             </PersonalBaseInfos>
           </PersonalInfosFooter>
         </PersonalInfos>
